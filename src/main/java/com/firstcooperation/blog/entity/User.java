d040a3ba41a239684bb06d1de28ae3bdc138ffa1
lureSky <867403822@qq.com>
2019-05-14 14:03:17 +0800

    修改了用户的ID为Int属性

--- src/main/java/com/firstcooperation/blog/entity/User.java
+++ src/main/java/com/firstcooperation/blog/entity/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    // 用户的ID（UUID）
+    // 用户的ID
     @TableId("user_id")
 	private String userId;
     // 用户的昵称
